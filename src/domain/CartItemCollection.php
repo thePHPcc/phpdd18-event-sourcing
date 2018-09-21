8d2b05c7d396273a5a05a265889769643e3766a6
Steve Schütze <sts@me-office365.de>
2018-09-21 17:47:48 +0200

    use global count method first

--- src/domain/CartItemCollection.php
+++ src/domain/CartItemCollection.php
@@ -17,7 +17,7 @@ class CartItemCollection implements \IteratorAggregate, \Countable
 
     public function count(): int
     {
-        return count($this->items);
+        return \count($this->items);
     }
 
     public function getIterator(): \ArrayIterator
